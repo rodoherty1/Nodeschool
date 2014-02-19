5c5ba4ca9009e88b1276d4a9403f86f681faa8c3
Rob O'D <rodoherty1@gmail.com>
2014-02-19 22:21:52 +0000

    Introducing Crypto

---
+++ stream-crypt.js
@@ -0,0 +1,9 @@
+var crypto = require('crypto');
+
+var passphrase = process.argv[2];
+
+var stream = crypto.createDecipher('aes256', passphrase);
+
+process.stdin.pipe(stream).pipe(process.stdout);
+
+
