b6bae95bfab5923e504845adfcf824439e622c70
Rob O'D <rodoherty1@gmail.com>
2014-02-20 22:32:46 +0000

    Crypto, tar, zlib and  through

--- stream-secretz.js
+++ stream-secretz.js
@@ -3,35 +3,28 @@ var zlib = require('zlib');
 var tar = require('tar');
 var through = require('through');
 
-var hash = crypto.createHash('md5', {encoding: 'hex'});
-
-var gunzip = zlib.createGunzip();
-var tr = through (write, end); 
-var parse = tar.Parse();
-
-var result = {};
-
-parse.on ('entry', function(contents) {
-	console.log('on entry');
-	result.md5 = contents.pipe(hash);
-	console.log(result.md5);
-});
-
-parse.on ('path', function(path) {
-	console.log('path');
-	result.path = path;
-});
-
-function write (buf) {
-}
-
-function end() {
-	console.log('end');
-	this.queue(result.md5 + ' ' + result.path + '\n');
-}
-
 var cipher = process.argv[2];
 var passphrase = process.argv[3];
 
-process.stdin.pipe(gunzip).pipe(parse).pipe(tr).pipe(process.stdout);
+var decipher = crypto.createDecipher(cipher, passphrase);
+var gunzip = zlib.createGunzip();
+var parse = tar.Parse();
+
+var path = {};
+
+parse.on ('entry', function(entry) {
+	if (entry.type == 'File') {
+		path = entry.path;
+		var hash = crypto.createHash('md5', {encoding: 'hex'});
+		var tr = through (write); 
+		entry.pipe(hash).pipe(tr).pipe(process.stdout);
+	}
+});
+
+
+function write (buf) {
+	this.queue(buf.toString() + ' ' + path + '\n');
+}
+
+process.stdin.pipe(decipher).pipe(gunzip).pipe(parse);
 
