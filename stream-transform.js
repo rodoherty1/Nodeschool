8a1b94210bf5c15fbb21a112ef4063944a2b8f96
Rob O'D <rodoherty1@gmail.com>
2014-03-14 23:38:46 +0000

    Through

--- stream-transform.js
+++ stream-transform.js
@@ -1,17 +1,13 @@
 var through = require('through');
 
-
-
 var write = function(buf) {
 	this.queue(buf.toString().toUpperCase());
 };
 
-var end = function(buf) {
+var end = function() {
 	this.queue(null);
 }
 
-
 var tr = through(write, end);
-
 
 process.stdin.pipe(tr).pipe(process.stdout);
