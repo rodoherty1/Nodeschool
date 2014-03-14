33d567552d8e661baea5224b6c059f5df89bef1f
Rob O'D <rodoherty1@gmail.com>
2014-03-14 23:36:51 +0000

    hasOwnProperty, Object.prototype.xxxx.call

--- functional-js-call.js
+++ functional-js-call.js
@@ -1,13 +1,25 @@
 
 
 function duckCount() {
-
-	return arguments.reduce(function(prev, current, index, arr) {
-		if (Object.prototype.hasOwnProperty.call(current, 'duck')) {
-			++prev || prev;
+	var args = Array.prototype.slice.call(arguments);
+	return args.reduce(function(prev, current, index, arr) {
+		if (Object.prototype.hasOwnProperty.call(current, 'quack')) {
+			prev++;
+		} 
+		return prev;
 	}, 0);
-
 }
+/*
+Official Solution
+=================
+
+  function duckCount() {
+    return Array.prototype.slice.call(arguments).filter(function(obj) {
+      return Object.prototype.hasOwnProperty.call(obj, 'quack')
+    }).length
+  }
+
+*/
 
 module.exports = duckCount
 
