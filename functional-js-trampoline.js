5ac3c81a5a9e8e5869d086a864708c0d13d2b1f6
Rob O'D <rodoherty1@gmail.com>
2014-05-14 16:35:40 +0100

    Trampolining

---
+++ functional-js-trampoline.js
@@ -0,0 +1,26 @@
+
+function repeat(operation, num) {
+	if (num < 0) {
+		return;
+	} else {
+		return function() {
+			operation();
+			return repeat(operation, --num);
+		};
+	}
+}
+
+function trampoline(fn) {
+	while (fn && typeof fn === 'function') {
+		fn = fn();
+	}
+
+	return;
+}
+
+module.exports = function (operation, num) {
+	return trampoline(function() {
+		return repeat(operation, num);
+	});
+}
+
